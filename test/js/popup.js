95bd7becb4c42d0bd2e8161155e4c9b070f62763
배준혁 <air8088@naver.com>
2021-04-30 14:43:07 +0900

    중간점검

---
+++ test/js/popup.js
@@ -0,0 +1,15 @@
+
+    function showPopup1() { window.open("License/TermsOfService.html", "a", "width=700, height=600, left=100, top=50"); }
+    
+
+    function showPopup2() { window.open("License/PrivacyPolicy.html", "a", "width=700, height=600, left=100, top=50"); }
+   
+
+    function showPopup3() { window.open("License/YouthProtectionPolicy.html", "a", "width=700, height=600, left=100, top=50"); }
+   
+
+    function showPopup4() { window.open("License/InappositeMail.html", "a", "width=700, height=600, left=100, top=50"); }
+    
+
+    function showPopup5() { window.open("로그인.html", "a", "width=1400, height=800, left=100, top=50"); }
+   
